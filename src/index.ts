e3e1c6951e25b5f8b81d21e374abc9b023c7dd78
Peter Ullrich <dotup.software@gmail.com>
2019-04-19 19:01:34 +0200

    exports

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,3 @@ export * from './Gpio/ConsoleGpio';
 export * from './Gpio/GpioFactory';
 export * from './Gpio/IGpio';
 export * from './Gpio/RaspberryGpio';
-export * from './Lamps/ILamp';
-export * from './Lamps/Lamp';
-export * from './Lamps/LampFactory';
-export * from './Lamps/LampResponsibility';
-export * from './Lamps/LampSet';
