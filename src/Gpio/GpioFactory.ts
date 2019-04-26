df18592967583fbe05f1dffdcdbbb3d04abd637e
Peter Ullrich <dotup.software@gmail.com>
2019-04-26 19:14:28 +0200

    factory

--- src/Gpio/GpioFactory.ts
+++ src/Gpio/GpioFactory.ts
@@ -3,12 +3,13 @@ import { ConsoleGpio } from './ConsoleGpio';
 import { IGpio } from './IGpio';
 import { RaspberryGpio } from './RaspberryGpio';
 import { Environment } from '../Environment.Pi';
+import { BinaryValue } from 'onoff';
 
 export namespace GpioFactory {
-  export function create(pin: number, useConsole: boolean = false): IGpio {
+  export function create(pin: number, useConsole: boolean = false, onValue: BinaryValue): IGpio {
 
     if (Environment.isPi() && !useConsole) {
-      return new RaspberryGpio(pin);
+      return new RaspberryGpio(pin, onValue);
     } else {
       return new ConsoleGpio(pin);
     }
